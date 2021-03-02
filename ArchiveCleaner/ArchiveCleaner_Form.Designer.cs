5eb6db1792da268428dbbfa09779a2907d5d7b94
Sven Mathing <sven.mathing@googlemail.com>
2021-03-02 22:46:09 +0100

    Projektdateien hinzufügen.

---
+++ ArchiveCleaner/ArchiveCleaner_Form.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace ArchiveCleaner
+{
+    partial class ArchiveCleaner_Form
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ArchiveCleaner_Form));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SelectPathButton = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DateTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FilesListBox = new System.Windows.Forms.ListBox();
+            this.DeleteAllButton = new System.Windows.Forms.Button();
+            this.DeleteSelectedButton = new System.Windows.Forms.Button();
+            this.CloseProgramButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.SearchButton);
+            this.groupBox1.Controls.Add(this.SelectPathButton);
+            this.groupBox1.Controls.Add(this.PathTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.DateTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 164);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parameter";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.SearchButton.Location = new System.Drawing.Point(248, 115);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(230, 34);
+            this.SearchButton.TabIndex = 5;
+            this.SearchButton.Text = "Suche passende Dokumente";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // SelectPathButton
+            // 
+            this.SelectPathButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectPathButton.Location = new System.Drawing.Point(552, 65);
+            this.SelectPathButton.Name = "SelectPathButton";
+            this.SelectPathButton.Size = new System.Drawing.Size(167, 37);
+            this.SelectPathButton.TabIndex = 4;
+            this.SelectPathButton.Text = "Verzeichnis wählen";
+            this.SelectPathButton.UseVisualStyleBackColor = true;
+            this.SelectPathButton.Click += new System.EventHandler(this.SelectPathButton_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PathTextBox.Location = new System.Drawing.Point(133, 70);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(413, 26);
+            this.PathTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Verzeichnis";
+            // 
+            // DateTextBox
+            // 
+            this.DateTextBox.Location = new System.Drawing.Point(133, 36);
+            this.DateTextBox.Name = "DateTextBox";
+            this.DateTextBox.Size = new System.Drawing.Size(147, 26);
+            this.DateTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "erstellt vor";
+            // 
+            // FilesListBox
+            // 
+            this.FilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilesListBox.FormattingEnabled = true;
+            this.FilesListBox.ItemHeight = 20;
+            this.FilesListBox.Location = new System.Drawing.Point(13, 199);
+            this.FilesListBox.Name = "FilesListBox";
+            this.FilesListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.FilesListBox.Size = new System.Drawing.Size(725, 344);
+            this.FilesListBox.TabIndex = 1;
+            // 
+            // DeleteAllButton
+            // 
+            this.DeleteAllButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DeleteAllButton.Location = new System.Drawing.Point(13, 586);
+            this.DeleteAllButton.Name = "DeleteAllButton";
+            this.DeleteAllButton.Size = new System.Drawing.Size(185, 35);
+            this.DeleteAllButton.TabIndex = 2;
+            this.DeleteAllButton.Text = "alle löschen";
+            this.DeleteAllButton.UseVisualStyleBackColor = true;
+            this.DeleteAllButton.Click += new System.EventHandler(this.DeleteAllButton_Click);
+            // 
+            // DeleteSelectedButton
+            // 
+            this.DeleteSelectedButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.DeleteSelectedButton.Location = new System.Drawing.Point(290, 586);
+            this.DeleteSelectedButton.Name = "DeleteSelectedButton";
+            this.DeleteSelectedButton.Size = new System.Drawing.Size(185, 35);
+            this.DeleteSelectedButton.TabIndex = 3;
+            this.DeleteSelectedButton.Text = "ausgewählte löschen";
+            this.DeleteSelectedButton.UseVisualStyleBackColor = true;
+            this.DeleteSelectedButton.Click += new System.EventHandler(this.DeleteSelectedButton_Click);
+            // 
+            // CloseProgramButton
+            // 
+            this.CloseProgramButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseProgramButton.Location = new System.Drawing.Point(553, 586);
+            this.CloseProgramButton.Name = "CloseProgramButton";
+            this.CloseProgramButton.Size = new System.Drawing.Size(185, 35);
+            this.CloseProgramButton.TabIndex = 4;
+            this.CloseProgramButton.Text = "Schließen";
+            this.CloseProgramButton.UseVisualStyleBackColor = true;
+            this.CloseProgramButton.Click += new System.EventHandler(this.CloseProgramButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "(c) 2021 Sven Mathing";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Consolas", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(562, 5);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(80, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "EUPL V1.2";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Consolas", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(648, 5);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(88, 17);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Sourcecode";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // ArchiveCleaner_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 633);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.CloseProgramButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DeleteSelectedButton);
+            this.Controls.Add(this.DeleteAllButton);
+            this.Controls.Add(this.FilesListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ArchiveCleaner_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ArchiveCleaner";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button SelectPathButton;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DateTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox FilesListBox;
+        private System.Windows.Forms.Button DeleteAllButton;
+        private System.Windows.Forms.Button DeleteSelectedButton;
+        private System.Windows.Forms.Button CloseProgramButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ ArchiveCleaner/ArchiveCleaner_Form.cs
@@ -0,0 +1,289 @@
+﻿/* Copyright (c) 2021 Sven Mathing
+ * Licensed under the EUPL, Version 1.2 or as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work except
+ * in compliance with the Licence. You may obtain a copy of the Licence at:
+ * http://joinup.ec.europa.eu/software/page/eupl Unless required by applicable law or agreed to in writing,
+ * software distributed under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchiveCleaner
+{
+    public partial class ArchiveCleaner_Form : Form
+    {
+        public ArchiveCleaner_Form()
+        {
+            InitializeComponent();
+
+            //default timespan is 10 years back
+            DateTextBox.Text = new DateTime((DateTime.Today.Year) - 10, 1, 1).ToShortDateString();
+        }
+
+        /// <summary>
+        /// closes the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseProgramButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// helps selecting a valid path by showing a folder browsing dialog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectPathButton_Click(object sender, EventArgs e)
+        {
+            
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result.Equals(DialogResult.OK))
+            {
+                //MessageBox.Show("verzeichnis:" + dialog.FileName);
+                PathTextBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// starts the search for files which fullfill the search criteria
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (PathTextBox.Text.Length > 0 && DateTextBox.Text.Length > 0)
+            {
+                //date and path are filled
+
+                //recursive search for files 
+                string[] allFiles = Directory.GetFiles(PathTextBox.Text, "*.*", System.IO.SearchOption.AllDirectories);
+                //MessageBox.Show("There are " + allFiles.Length + " files..."+ allFiles[0]+ " " + File.GetCreationTime(allFiles[0]));
+
+                //now go through the list and find the one older than the date
+                List<string> filesToDelete = new List<string>();
+                DateTime fileCreationDate;
+                DateTime searchDate = DateTime.Parse(DateTextBox.Text);
+                FilesListBox.Items.Clear();
+                foreach (string file in allFiles)
+                {
+                    //get Fileproperties
+                    fileCreationDate = File.GetCreationTime(file);
+                    if (fileCreationDate < searchDate)
+                    {
+                        filesToDelete.Add(file);
+                        FilesListBox.Items.Add(file);
+                    }
+                }
+                MessageBox.Show("Es gibt " + filesToDelete.Count + " Dateien, die den Suchparametern entsprechen!");
+            }
+            else
+            {
+                MessageBox.Show("Bitte zuerst ein Verzeichnis und ein Datum wählen!");
+                return;
+            }
+        }
+
+        /// <summary>
+        /// deletes all files in the Listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteAllButton_Click(object sender, EventArgs e)
+        {
+            List<string> filesToDelete;
+            filesToDelete = FilesListBox.Items.OfType<string>().ToList();
+            int filecount = FilesListBox.Items.Count;
+            if (filesToDelete.Count > 0)
+            {
+                if (MessageBox.Show("Wollen Sie wirklich "+filecount+" Datei(en) und Verzeichnisse löschen?", "Warnung", MessageBoxButtons.YesNo, MessageBoxIcon.Stop).Equals(DialogResult.Yes))
+                {
+                    //YES...delete the files
+                    if (MessageBox.Show("Wollen Sie wirklich löschen? " + Environment.NewLine
+                        + "Nach 'Ja' werden ALLE DIESE DATEIEN GELÖSCHT UND KÖNNEN NUR ÜBER DEN PAPIERKORB WIEDERHERGESTELLT WERDEN!", "Warnung", MessageBoxButtons.YesNo, MessageBoxIcon.Stop).Equals(DialogResult.Yes))
+                    {
+                        foreach (string file in filesToDelete)
+                        {
+                            DeleteFileAndFolder(file);
+                        }
+                    }
+                }
+                else
+                {
+                    //Do not delete the files...
+                    MessageBox.Show("Dateien wurden NICHT gelöscht!", "Entwarnung", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// deletes all selected files from the listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteSelectedButton_Click(object sender, EventArgs e)
+        {
+            List<string> filesToDelete;
+            filesToDelete = FilesListBox.SelectedItems.OfType<string>().ToList();
+
+            //check if there are some selected files
+            int filecount = FilesListBox.SelectedItems.Count;
+            if (filecount > 0)
+            {
+                if (MessageBox.Show("Wollen Sie wirklich " + filecount + " Datei(en) und zugehörige leere Verzeichnisse löschen?", "Warnung", MessageBoxButtons.YesNo, MessageBoxIcon.Stop).Equals(DialogResult.Yes))
+                {
+                    //YES...delete the files
+                    if (MessageBox.Show("Wollen Sie wirklich löschen? " + Environment.NewLine
+                        + "Nach 'Ja' werden ALLE DIESE DATEIEN GELÖSCHT UND KÖNNEN NUR ÜBER DEN PAPIERKORB WIEDERHERGESTELLT WERDEN!", "Warnung", MessageBoxButtons.YesNo, MessageBoxIcon.Stop).Equals(DialogResult.Yes))
+                    {
+                        foreach (string file in filesToDelete)
+                        {
+                            DeleteFileAndFolder(file);
+                        }
+                    }
+                }
+                else
+                {
+                    //Do not delete the files...
+                    MessageBox.Show("Dateien wurden NICHT gelöscht!", "Entwarnung", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// deletes one file and maybe its folder (if it is empty)
+        /// The files/folder are deleted using the trash bin for recovery
+        /// </summary>
+        /// <param name="filename">the file with complete path to delete</param>
+        private void DeleteFileAndFolder(string filename)
+        {            
+            string folder = System.IO.Path.GetDirectoryName(filename);
+
+            //try to delete the file and then the folder
+            try
+            {
+                Console.Write("Lösche Datei " + filename+ " ... ");
+                //deleting without trash bin
+                //File.Delete(filename);    
+
+                //deleting with trash bin
+                FileSystem.DeleteFile(filename, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+
+                Console.WriteLine("fertig!");
+                //lösche EIntrag aus der Listbox
+                FilesListBox.Items.Remove(filename);
+            }catch (Exception ex)
+            {
+                //error deleting the file
+                Console.WriteLine("Datei konnte nicht gelöscht werden ["+ex.Message+"]");
+            }
+            
+            //delete the folder if it is empty
+            Console.Write(String.Format("Verzeichnis {0} ", folder));
+            if (IsEmpty(folder))
+            {
+                try {
+                    Console.Write("ist leer und wird gelöscht... ");
+                    //deleting without trash bin
+                    //Directory.Delete(folder); 
+
+                    //deleting with trash bin
+                    FileSystem.DeleteDirectory(folder, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+
+                    Console.WriteLine("fertig!");
+                }
+                catch (Exception ex)
+                {
+                    //error deleting the file
+                    Console.WriteLine("Verzeichnis konnte nicht gelöscht werden [" + ex.Message + "]");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ist noch nicht leer und wird nicht gelöscht!");
+            }
+        }
+
+        /// <summary>
+        /// checks if an folder is empty
+        /// </summary>
+        /// <param name="folderPath">the folder to check</param>
+        /// <returns></returns>
+        private static bool IsEmpty(string folderPath)
+        {
+            bool allSubFoldersEmpty = true;
+            //check every subfolder
+            foreach (var subFolder in Directory.EnumerateDirectories(folderPath))
+            {
+                if (IsEmpty(subFolder))
+                {
+                    //Console.Write($"[Empty Subfolder: {subFolder}] ");
+                }
+                else
+                {
+                    allSubFoldersEmpty = false;
+                }
+            }
+
+            if (allSubFoldersEmpty && !HasFiles(folderPath))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        /// <summary>
+        /// checks if in a folder are some files
+        /// </summary>
+        /// <param name="folderPath">the folder to chekc</param>
+        /// <returns>true if files are found</returns>
+        private static bool HasFiles(string folderPath)
+        {
+            return Directory.EnumerateFiles(folderPath).Any();
+        }
+
+        /// <summary>
+        /// opens the link for the linklabel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            this.linkLabel1.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12");
+        }
+
+        /// <summary>
+        /// opens the link for the linklabel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            this.linkLabel1.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("https://github.com/SvenMathing/ArchiveCleaner");
+        }
+    }
+}
